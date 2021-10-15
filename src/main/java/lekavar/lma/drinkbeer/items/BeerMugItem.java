f8757892c16f1cb9065c77d2a721321e12ee8ef1
Lekavar <89635694+Lekavar@users.noreply.github.com>
2021-10-15 17:10:02 +0800

    Return empty mug back after using

--- src/main/java/lekavar/lma/drinkbeer/items/BeerMugItem.java
+++ src/main/java/lekavar/lma/drinkbeer/items/BeerMugItem.java
@@ -1,9 +1,12 @@
 package lekavar.lma.drinkbeer.items;
 
+import lekavar.lma.drinkbeer.DrinkBeer;
 import lekavar.lma.drinkbeer.registry.ItemRegistry;
 import lekavar.lma.drinkbeer.registry.SoundEventRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -12,6 +15,7 @@ import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
+import net.minecraftforge.items.ItemHandlerHelper;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -34,5 +38,17 @@ public class BeerMugItem extends BlockItem {
         }
         String hunger = String.valueOf(asItem().getFoodProperties().getNutrition());
         tooltip.add(new TranslationTextComponent("drinkbeer.restores_hunger").setStyle(Style.EMPTY.applyFormat(TextFormatting.BLUE)).append(hunger));
+    }
+
+    @Override
+    public ItemStack finishUsingItem(ItemStack stack, World world, LivingEntity player) {
+        ItemStack itemStack = super.finishUsingItem(stack, world, player);
+        if (player instanceof PlayerEntity && ((PlayerEntity) player).isCreative()) {
+            return itemStack;
+        } else {
+            ItemStack emptyMugItemStack = new ItemStack(ItemRegistry.emptyBeerMug.get(), 1);
+            ItemHandlerHelper.giveItemToPlayer((PlayerEntity) player, emptyMugItemStack);
+            return itemStack;
+        }
     }
 }
